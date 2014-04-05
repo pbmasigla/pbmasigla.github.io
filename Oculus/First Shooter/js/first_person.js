2be17616ad8a121809596c96dae9c66a95f38675
pbmasigla <patricia.masigla@gmail.com>
2014-04-05 18:54:27 -0400

    change

--- Oculus/First Shooter/js/first_person.js
+++ Oculus/First Shooter/js/first_person.js
@@ -32,119 +32,166 @@ for(var i = 0; i < 130; i++){
 }
 
 
-function initScene() {
-  clock = new THREE.Clock();
-  mouse = new THREE.Vector2(0, 0);
+var updateFcts = [];
+var scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0xd0e0f0, 0.0025);
 
-  windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
-  aspectRatio = window.innerWidth / window.innerHeight;
-  
-  scene = new THREE.Scene();  
+var renderer = new THREE.WebGLRenderer({
+    antialias: false
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-  camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 10000);
-  camera.useQuaternion = true;
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 3000)
+camera.position.y = 80
 
-  camera.position.set(100, 150, 100);
-  camera.lookAt(scene.position);
+var light = new THREE.HemisphereLight(0xfffff0, 0x101020, 1.25);
+light.position.set(0.75, 1, 0.25);
+scene.add(light);
 
-  // Initialize the renderer
-  renderer = new THREE.WebGLRenderer({antialias:true});
-  renderer.setClearColor(0xdbf7ff);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+var material = new THREE.MeshBasicMaterial({
+    color: 0x101018
+})
+var geometry = new THREE.PlaneGeometry(2000, 2000)
+var plane = new THREE.Mesh(geometry, material);
+plane.rotation.x = -90 * Math.PI / 180;
+scene.add(plane);
 
-   scene.fog = new THREE.Fog(0xdbf7ff, 300, 700);
+var city = new THREEx.ProceduralCity()
+scene.add(city)
 
-  element = document.getElementById('viewport');
-  element.appendChild(renderer.domElement);
+var controls = new THREE.FirstPersonControls(camera);
+controls.movementSpeed = 20;
+controls.lookSpeed = 0.05;
+controls.lookVertical = true;
+updateFcts.push(function (delta, now) {
+    controls.update(delta);
+})
 
-  controls = new THREE.OrbitControls(camera);
-}
+updateFcts.push(function () {
+    renderer.render(scene, camera);
+})
 
+var lastTimeMsec = null
+requestAnimationFrame(function animate(nowMsec) {
+    // keep looping
+    requestAnimationFrame(animate);
+    // measure time
+    lastTimeMsec = lastTimeMsec || nowMsec - 1000 / 60
+    var deltaMsec = Math.min(200, nowMsec - lastTimeMsec)
+    lastTimeMsec = nowMsec
+    // call each update function
+    updateFcts.forEach(function (updateFn) {
+        updateFn(deltaMsec / 1000, nowMsec / 1000)
+    })
+})
 
-function initLights(){
+/*Regular Code*/
+var THREEx = THREEx || {}
 
-  ambient = new THREE.AmbientLight(0x222222);
-  scene.add(ambient);
+THREEx.ProceduralCity = function () {
+    // build the base geometry for each building
+    var geometry = new THREE.CubeGeometry(1, 1, 1);
+    // translate the geometry to place the pivot point at the bottom instead of the center
+    geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0.5, 0));
+    // get rid of the bottom face - it is never seen
+    geometry.faces.splice(3, 1);
+    geometry.faceVertexUvs[0].splice(3, 1);
+    // change UVs for the top face
+    // - it is the roof so it wont use the same texture as the side of the building
+    // - set the UVs to the single coordinate 0,0. so the roof will be the same color
+    //   as a floor row.
+    geometry.faceVertexUvs[0][2][0].set(0, 0);
+    geometry.faceVertexUvs[0][2][1].set(0, 0);
+    geometry.faceVertexUvs[0][2][2].set(0, 0);
+    geometry.faceVertexUvs[0][2][3].set(0, 0);
+    // buildMesh
+    var buildingMesh = new THREE.Mesh(geometry);
 
-  point = new THREE.DirectionalLight( 0xffffff, 1, 0, Math.PI, 1 );
-  point.position.set( -250, 250, 150 );
-  
-  scene.add(point);
-}
+    // base colors for vertexColors. light is for vertices at the top, shaddow is for the ones at the bottom
+    var light = new THREE.Color(0xffffff)
+    var shadow = new THREE.Color(0x303050)
 
-var floorTexture;
-function initGeometry(){
+    var cityGeometry = new THREE.Geometry();
+    for (var i = 0; i < 20000; i++) {
+        // put a random position
+        buildingMesh.position.x = Math.floor(Math.random() * 200 - 100) * 10;
+        buildingMesh.position.z = Math.floor(Math.random() * 200 - 100) * 10;
+        // put a random rotation
+        buildingMesh.rotation.y = Math.random() * Math.PI * 2;
+        // put a random scale
+        buildingMesh.scale.x = Math.random() * Math.random() * Math.random() * Math.random() * 50 + 10;
+        buildingMesh.scale.y = (Math.random() * Math.random() * Math.random() * buildingMesh.scale.x) * 8 + 8;
+        buildingMesh.scale.z = buildingMesh.scale.x
 
-  floorTexture = new THREE.ImageUtils.loadTexture( "textures/tile.jpg" );
-  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
-  floorTexture.repeat.set( 50, 50 );
-  floorTexture.anisotropy = 32;
-
-  var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, transparent:true, opacity:0.80 } );
-  var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  floor.rotation.x = -Math.PI / 2;
-
-  scene.add(floor);
-
-  // add some boxes.
-  var boxTexture = new THREE.ImageUtils.loadTexture( "textures/blue_blue.jpg" );
-  for(var i = 0; i < 200; i++){
-    var material = new THREE.MeshLambertMaterial({ emissive:0x505050, map: boxTexture, color: 0xffffff});
-    
-    var height = Math.random() * 150+10;
-    var width = Math.random() * 20 + 2;
-    
-    var box = new THREE.Mesh( new THREE.CubeGeometry(width, height, width), material);
-
-    box.position.set(Math.random() * 1000 - 500, height/2 ,Math.random() * 1000 - 500);
-    box.rotation.set(0, Math.random() * Math.PI * 2, 0);
-    
-    boxes.push(box);
-    scene.add(box);
-  }
-
-  var coreTexture = new THREE.ImageUtils.loadTexture( "textures/purple_blue.jpg" );
-  for(var i = 0; i < 50; i++){
-    var material = new THREE.MeshLambertMaterial({ emissive:0x505050, map: coreTexture, color: 0xffffff});
-    
-    var height = Math.random() * 100+30;
-    
-    var box = new THREE.Mesh( new THREE.CubeGeometry(height, height, height), material);
-
-    box.position.set(Math.random() * 1000 - 500, Math.random() * 150 - 300 ,Math.random() * 1000 - 500);
-    box.rotation.set(Math.random() * Math.PI * 2, Math.random() * Math.PI * 2, Math.random() * Math.PI * 2);
-    
-    core.push(box);
-    scene.add(box);
-  }
-
-  for(var i = 0; i < 100; i++){
-    var material = new THREE.MeshLambertMaterial({ emissive:0x008000, color: 0x00FF00});
-    
-    var size = Math.random() * 15+3;
-    
-    var box = new THREE.Mesh( new THREE.CubeGeometry(size, size*0.1, size*0.1), material);
-
-    box.position.set(Math.random() * 1000 - 500, Math.random() * 100 + 100 ,Math.random() * 1000 - 500);
-    //box.rotation.set(Math.random() * Math.PI * 2, Math.random() * Math.PI * 2, Math.random() * Math.PI * 2);
-    
-    var speedVector;
-    if(Math.random() > 0.5){
-      speedVector = new THREE.Vector3(0, 0, Math.random() * 1.5 + 0.5);
-      box.rotation.y = Math.PI / 2;
-    } else {
-      speedVector = new THREE.Vector3(Math.random() * 1.5 + 0.5, 0, 0);
+        // establish the base color for the buildingMesh
+        var value = 1 - Math.random() * Math.random();
+        var baseColor = new THREE.Color().setRGB(value + Math.random() * 0.1, value, value + Math.random() * 0.1);
+        // set topColor/bottom vertexColors as adjustement of baseColor
+        var topColor = baseColor.clone().multiply(light);
+        var bottomColor = baseColor.clone().multiply(shadow);
+        // set .vertexColors for each face
+        var geometry = buildingMesh.geometry;
+        for (var j = 0, jl = geometry.faces.length; j < jl; j++) {
+            if (j === 2) {
+                // set face.vertexColors on root face
+                geometry.faces[j].vertexColors = [baseColor, baseColor, baseColor, baseColor];
+            } else {
+                // set face.vertexColors on sides faces
+                geometry.faces[j].vertexColors = [topColor, bottomColor, bottomColor, topColor];
+            }
+        }
+        // merge it with cityGeometry - very important for performance
+        THREE.GeometryUtils.merge(cityGeometry, buildingMesh);
     }
 
-    dataPackets.push({
-      obj: box,
-      speed: speedVector
-    });
-    scene.add(box);
-  }
-}
+    // generate the texture
+    var texture = new THREE.Texture(generateTextureCanvas());
+    texture.anisotropy = renderer.getMaxAnisotropy();
+    texture.needsUpdate = true;
 
+    // build the mesh
+    var material = new THREE.MeshLambertMaterial({
+        map: texture,
+        vertexColors: THREE.VertexColors
+    });
+    var mesh = new THREE.Mesh(cityGeometry, material);
+    return mesh
+
+    function generateTextureCanvas() {
+        // build a small canvas 32x64 and paint it in white
+        var canvas = document.createElement('canvas');
+        canvas.width = 32;
+        canvas.height = 64;
+        var context = canvas.getContext('2d');
+        // plain it in white
+        context.fillStyle = '#ffffff';
+        context.fillRect(0, 0, 32, 64);
+        // draw the window rows - with a small noise to simulate light variations in each room
+        for (var y = 2; y < 64; y += 2) {
+            for (var x = 0; x < 32; x += 2) {
+                var value = Math.floor(Math.random() * 64);
+                context.fillStyle = 'rgb(' + [value, value, value].join(',') + ')';
+                context.fillRect(x, y, 2, 1);
+            }
+        }
+
+        // build a bigger canvas and copy the small one in it
+        // This is a trick to upscale the texture without filtering
+        var canvas2 = document.createElement('canvas');
+        canvas2.width = 512;
+        canvas2.height = 1024;
+        var context = canvas2.getContext('2d');
+        // disable smoothing
+        context.imageSmoothingEnabled = false;
+        context.webkitImageSmoothingEnabled = false;
+        context.mozImageSmoothingEnabled = false;
+        // then draw the image
+        context.drawImage(canvas, 0, 0, canvas2.width, canvas2.height);
+        // return the just built canvas2
+        return canvas2;
+    }
+}
 
 function init(){
 
@@ -408,3 +455,4 @@ window.onload = function() {
   init();
   animate();
 }
+
