3b8838c2bf7b9c2d6dac871f182adf294478e0bb
pbmasigla <pbmasigla@Patricias-MacBook-Pro.local>
2014-09-18 14:37:23 -0400

    added deck of cards to puzzles

--- puzzles/js/puzzles.js
+++ puzzles/js/puzzles.js
@@ -11,6 +11,12 @@
       problem: 'http://algorithmsquestionsforinterviews.blogspot.com/2013/11/isomorphic-strings.html',
       number: '1',
   },
+  {
+      name: 'Deck of Cards',
+      app_link: 'deckOfCards',
+      problem: 'http://www.glassdoor.com/Interview/CustomInk-Software-Engineer-Interview-Questions-EI_IE152733.0,9_KO10,27.htm',
+      number: '2',
+  },
   ];
 
 })();
