fe18445f1d46601516898e4ebb92224e5d13de7c
pbmasigla <pbmasigla@patricias-mbp.home>
2014-09-17 23:23:20 -0400

    updated letterMapper link

--- puzzles/js/puzzles.js
+++ puzzles/js/puzzles.js
@@ -8,15 +8,9 @@
   var pattysPuzzles = [{
       name: 'Letter Mapper Puzzle',
       app_link: 'letterMapper',
-      raw_link: 'https://github.com/pbmasigla/pattyscodingpuzzles.github.io/tree/master/letterMapper',
       problem: 'http://algorithmsquestionsforinterviews.blogspot.com/2013/11/isomorphic-strings.html',
       number: '1',
   },
-  {
-      name: 'Letter Mapper Puzzle',
-      app_link: '',
-      raw_link: '',
-      number: '2',
-  }];
+  ];
 
 })();
