09b1dcb38fcf977877cdee0322439056f5cc23d2
Patricia Masigla <patricia@socialtables.com>
2016-03-08 11:17:53 -0500

    REVERT THIS LATEER

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-import React from 'react';
-import Root from "./root";
-import { createHistory } from "history";
-
-ReactDOM.render(
-	<Root history={ createHistory() } />,
-	document.getElementById("app")
-);
