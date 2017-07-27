110e9b115bcaf5c1f1494591dfa38897992dbdfd
Patricia Masigla <patricia@socialtables.com>
2017-07-27 10:07:26 -0400

    Update year

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import "./footer.less";
 export default function Footer() {
 	return (
 		<div className="footer__container">
-			<div className="footer__content">@PM 2016</div>
+			<div className="footer__content">@PM 2017</div>
 		</div>
 	);
 };
