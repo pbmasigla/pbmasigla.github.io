cd5b1b72f7df5e15abcb36bb3560a1a5f0bc6601
Patricia Masigla <patricia.masigla@gmail.com>
2019-01-09 16:03:28 -0500

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import "./footer.less";
 export default function Footer() {
 	return (
 		<div className="footer__container">
-			<div className="footer__content">@PM 2018</div>
+			<div className="footer__content">@PM 2019</div>
 		</div>
 	);
 };
